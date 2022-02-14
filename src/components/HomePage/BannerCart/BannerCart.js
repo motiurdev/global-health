c66b8a35d281e515d4b765af80f6c5b79a33bf50
motiurdev <motiurrahmanmisu@gmail.com>
2022-02-14 10:02:00 +0600

    banner-cart

--- src/components/HomePage/BannerCart/BannerCart.js
+++ src/components/HomePage/BannerCart/BannerCart.js
@@ -8,17 +8,17 @@ const BannerCart = () => {
             <div class="row banner-card">
                 <div class="col-12 col-md-4 banner-card-1 ps-4">
                     <h4>Professional staff</h4>
-                    <p className='py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis semper felis.</p>
+                    <p className='py-2'>The ADA has all the tools, resources, and information you need to grow your practice.</p>
                     <Link to="/appointment" className='banner-cart-btn'>Read More</Link>
                 </div>
                 <div class="col-12 col-md-4 banner-card-2 ps-4">
-                    <h4>Professional staff</h4>
-                    <p className='py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis semper felis.</p>
+                    <h4>patient care</h4>
+                    <p className='py-2'>The ADA has all the tools, resources, and information you need to grow your practice.</p>
                     <Link to="/appointment" className='banner-cart-btn'>Read More</Link>
                 </div>
                 <div class="col-12 col-md-4 banner-card-3 ps-4">
-                    <h4>Professional staff</h4>
-                    <p className='py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis semper felis.</p>
+                    <h4>Dentist workforce</h4>
+                    <p className='py-2'>The ADA has all the tools, resources, and information you need to grow your practice.</p>
                     <Link to="/appointment" className='banner-cart-btn'>Read More</Link>
                 </div>
             </div>
